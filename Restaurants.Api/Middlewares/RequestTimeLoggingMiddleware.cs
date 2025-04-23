d1b9e5c989ba210684e352573b5c85915057c535
Ahmed Ragab <ragabali@std.mans.edu.eg>
2025-04-23 20:40:46 +0200

    Implement RequestTimeLoggingMiddleware

---
+++ Restaurants.Api/Middlewares/RequestTimeLoggingMiddleware.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Connections.Features;
+using System.Diagnostics;
+
+namespace Restaurants.Api.Middlewares;
+
+public class RequestTimeLoggingMiddleware(ILogger<RequestTimeLoggingMiddleware> logger) : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        var timer =  Stopwatch.StartNew();
+			await next.Invoke(context);
+        timer.Stop();
+        
+        if(timer.ElapsedMilliseconds / 1000 > 4)
+        {
+            logger.LogInformation("Request {[Verb]} at {Path} took {Time} ms",
+                context.Request.Method, context.Request.Path, timer.ElapsedMilliseconds);
+        }
+    }
+}
