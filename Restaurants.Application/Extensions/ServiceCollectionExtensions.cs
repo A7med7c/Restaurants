6700aadfa0cb4dc0e444a1805052b8d7a85ef841
Ahmed Ragab <ragabali@std.mans.edu.eg>
2025-04-29 18:15:56 +0300

    changes

--- Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,29 +1,27 @@
 ﻿using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
-using Restaurants.Application.Restaurants;
 using Restaurants.Application.Users;
-namespace Restaurants.Application.Extensions
+namespace Restaurants.Application.Extensions;
+
+// group all internal services in application and regisered by add infra extension method
+public static class ServiceCollectionExtensions
 {
-    // group all internal services in application and regisered by add infra extension method
-    public static class ServiceCollectionExtensions
+    public static void AddApplication(this IServiceCollection services)
     {
-        public static void AddApplication(this IServiceCollection services)
-        {
-            // register IUserContext 
-            services.AddScoped<IUserContext, UserContext>();
-            // to be able to get httpcontext information
-            services.AddHttpContextAccessor();
+        // register IUserContext 
+        services.AddScoped<IUserContext, UserContext>();
+        // to be able to get httpcontext information
+        services.AddHttpContextAccessor();
 
-            var applicationassembly = typeof(ServiceCollectionExtensions).Assembly;
+        var applicationassembly = typeof(ServiceCollectionExtensions).Assembly;
 
-            services.AddAutoMapper(applicationassembly);
-           
-            services.AddValidatorsFromAssembly(applicationassembly)
-                .AddFluentValidationAutoValidation();
+        services.AddAutoMapper(applicationassembly);
+       
+        services.AddValidatorsFromAssembly(applicationassembly)
+            .AddFluentValidationAutoValidation();
 
-            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(applicationassembly));
-        }
-
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(applicationassembly));
     }
+
 }
