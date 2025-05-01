ed63f2079a193a172e3d9e798985e483d5f5d513
Ahmed Ragab <ragabali@std.mans.edu.eg>
2025-05-01 19:31:50 +0300

    UnAssign role from User

--- Restaurants.Api/Controllers/IdentityController.cs
+++ Restaurants.Api/Controllers/IdentityController.cs
@@ -3,17 +3,20 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Restaurants.Application.Users.Commands.AssignUserRole;
+using Restaurants.Application.Users.Commands.UnAssignUserRole;
 using Restaurants.Application.Users.Commands.UpdateUserDetails;
 using Restaurants.Domain.Constants;
 
 namespace Restaurants.Api.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/identity")]
 [ApiController]
 public class IdentityController(IMediator mediator) : ControllerBase
 {
     [HttpPatch("user")]
     [Authorize]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> UpdateUserDetails(UpdateUseDetailsCommand command)
     {
         await mediator.Send(command);
@@ -23,6 +26,16 @@ public class IdentityController(IMediator mediator) : ControllerBase
     [HttpPost("userRole")]
     [Authorize(Roles = UserRoles.Admin)]
     public async Task<IActionResult> AssignUserRole(AssignUserRoleCommand command)
+    {
+        await mediator.Send(command);
+        return NoContent();
+    }
+
+    [HttpDelete("userRole")]
+    [Authorize(Roles = UserRoles.Admin)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> UnAssignUserRole(UnAssignUserRoleCommand command)
     {
         await mediator.Send(command);
         return NoContent();
