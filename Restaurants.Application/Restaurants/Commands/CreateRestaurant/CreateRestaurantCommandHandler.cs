60ed55c1c864c782f3b525aae065dbf1d76129f2
Ahmed Ragab <ragabali@std.mans.edu.eg>
2025-05-06 07:03:38 +0300

    edit ownerid in create restaurant

--- Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
+++ Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
@@ -1,22 +1,26 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Restaurants.Application.Users;
 using Restaurants.Domain.Entities;
 using System.ComponentModel.DataAnnotations;
 
 namespace Restaurants.Application.Restaurants.Commands.CreateRestaurant;
 public class CreateRestaurantCommandHandler(ILogger<CreateRestaurantCommandHandler> logger,
-    IMapper mapper , IRestaurantsRepository restaurantsRepository) : IRequestHandler<CreateRestaurantCommand, int>
+    IMapper mapper,
+    IRestaurantsRepository restaurantsRepository,
+    IUserContext userContext) : IRequestHandler<CreateRestaurantCommand, int>
 {
     public async Task<int> Handle(CreateRestaurantCommand request, CancellationToken cancellationToken)
     {
+        var currentUser =  userContext.GetCurrentUser();
+
         //@ => make serilog print details of request
-        logger.LogInformation("Creating a new Restaurant{@Restaurant}", request);
-       
-        if (string.IsNullOrEmpty(request.OwnerId))
-            throw new ValidationException("OwnerId must be set by the controller.");
+        logger.LogInformation("{UserEmail} [{UserId}] Creating a new Restaurant{@Restaurant}",
+            currentUser.Email, currentUser.Id, request);
 
         var restaurant = mapper.Map<Restaurant>(request);
+        restaurant.OwnerId = currentUser.Id;
 
         int id = await restaurantsRepository.CreateAsync(restaurant);
         return id;
